2cf63240d5751e9983365955c0ac10f5a65ab24c
Chaithanya <chaitu1325@gmail.com>
2025-05-14 15:10:58 +0530

    Initial setup

---
+++ src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { EducationComponent } from './components/education/education.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'experience', redirectTo: 'portfolio', pathMatch: 'full' }, // Redirect old experience URL to portfolio
+  { path: '**', redirectTo: '' }
+];
---
+++ src/app/components/education/education.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-education',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './education.component.html',
+  styleUrl: './education.component.scss'
+})
+export class EducationComponent {
+
+}
